147c7f99f5b63750a2922f8804928484d58d01a1
Joshua Price <joshuapricew@gmail.com>
2022-12-19 00:45:08 +0000

    Add struct_example

---
+++ the-rust-book/struct_example/src/main.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("{:?}", &rect);
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
+}
