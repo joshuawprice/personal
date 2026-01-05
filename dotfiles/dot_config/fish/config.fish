e030c93721ecb8a3689f71aab2dee61c8ce97d82
Joshua Price <git@jprice.uk>
2026-01-05 18:09:06 +0000

    Set fish prompt with starship

--- dotfiles/dot_config/fish/config.fish
+++ dotfiles/dot_config/fish/config.fish
@@ -3,6 +3,12 @@
 if status is-interactive
     # Disable the default fish greeting
     set fish_greeting
+
+    if command -v starship >/dev/null
+        starship init fish | source
+    else
+        echo "`starship' not installed, falling back to default prompt." >&2
+    end
 end
 
 # vim: sts=4 sw=4 et
