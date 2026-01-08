1c299fd26774e7e9941c69e7d86a339863e67187
Joshua Price <git@jprice.uk>
2026-01-08 18:10:58 +0000

    Add warning if bat is not installed on first run

--- dotfiles/dot_config/fish/functions/cat.fish
+++ dotfiles/dot_config/fish/functions/cat.fish
@@ -1,7 +1,21 @@
-if command -v bat >/dev/null
-    function cat --wraps bat
-        command bat $argv
+# This file is sourced either on first tab completion, or when the command itself is run.
+# Warn once if the command is not installed.
+if ! command -v bat >/dev/null
+    echo -n "`bat' not installed, falling back to cat." >&2
+
+    # If this file is sourced by tab completion, the entire commandline gets
+    # broken by the echo above. Therefore go down the amount needed, and
+    # reprint it.
+    for i in $(seq 1 $(fish_prompt | string split \n | count))
+        echo ""
     end
+    commandline -f repaint
+
+    return
+end
+
+function cat --wraps bat
+    command bat $argv
 end
 
 # vim: sts=4 sw=4 et
