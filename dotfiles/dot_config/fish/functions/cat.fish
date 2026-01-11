9abcdfb79540c7a4bc9dacf6e2ba6bf71622a877
Joshua Price <git@jprice.uk>
2026-01-11 22:04:33 +0000

    Improve warning if bat is not installed

--- dotfiles/dot_config/fish/functions/cat.fish
+++ dotfiles/dot_config/fish/functions/cat.fish
@@ -1,21 +1,28 @@
-# This file is sourced either on first tab completion, or when the command itself is run.
-# Warn once if the command is not installed.
-if ! command -v bat >/dev/null
-    echo -n "`bat' not installed, falling back to cat." >&2
-
-    # If this file is sourced by tab completion, the entire commandline gets
-    # broken by the echo above. Therefore go down the amount needed, and
-    # reprint it.
-    for i in $(seq 1 $(fish_prompt | string split \n | count))
-        echo ""
-    end
-    commandline -f repaint
-
-    return
-end
-
 function cat --wraps bat
-    command bat $argv
+    # When fish is autoloading the completion for cat, it actually runs the
+    # cat command, inadvertently running this very function.
+    # To avoid this counting as the first invocation, test for it and just
+    # return normal cat.
+    #
+    # This appears to return "fish" when in the completions/ file, but cat
+    # otherwise when invoking the cat command.
+    if [ $(status current-command) = "fish" ]
+        command cat $argv
+        return
+    end
+
+    if command -q bat
+        command bat $argv
+        return
+    end
+
+    if not set -q __cat_bat_not_found
+        set -g __cat_bat_not_found 0
+        echo "`bat' not found, falling back to cat." >&2
+    end
+
+    command cat $argv
 end
+
 
 # vim: sts=4 sw=4 et
