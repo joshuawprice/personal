e209e33bcf8bdf884bbcb9a2bd13ca2f0f5cce43
Joshua Price <git@jprice.uk>
2026-03-12 18:23:11 +0000

    Add TabButton component

--- learning-react/01-starting-project/src/App.jsx
+++ learning-react/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
   return (
@@ -14,6 +15,15 @@ function App() {
               <CoreConcept {...concept} />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
