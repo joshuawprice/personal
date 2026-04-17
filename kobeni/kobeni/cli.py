733d3c2dc7e587f0f3a5ae8bc7513a0067452914
Joshua Price <git@jprice.uk>
2026-04-17 18:52:17 +0100

    Handle sigterm to shutdown
    Discord.py only handles sigint by default. This adds sigterm handling.
    
    Also, by accident happens to fix the slow bot shutdown when the bot is
    connected to a voice channel.

--- kobeni/kobeni/cli.py
+++ kobeni/kobeni/cli.py
@@ -1,8 +1,11 @@
 # TODO:
 # - Handle the exceptions listed here: https://discordpy.readthedocs.io/en/stable/api.html#discord.Client.connect
 
+import asyncio
+import functools
 import logging
 import os
+import signal
 
 import discord
 from discord.ext import commands
@@ -15,10 +18,22 @@ logger = logging.getLogger(__name__)
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
-
         super().__init__(*args, **kwargs)
 
+    def handle_shutdown_event(self, signal: str):
+        logger.info(f"{signal} received, shutting down...")
+        asyncio.create_task(self.close())
+
     async def setup_hook(self):
+        # Setup signals to shutdown properly. discord.py only handles SIGINT by default.
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(
+            signal.SIGINT, functools.partial(self.handle_shutdown_event, "SIGINT")
+        )
+        loop.add_signal_handler(
+            signal.SIGTERM, functools.partial(self.handle_shutdown_event, "SIGTERM")
+        )
+
         await self.add_cog(Mumble(self))
 
     async def close(self):
