d0392381b89c597a6ff7282f553d2d30d28a0d68
Joshua Price <git@jprice.uk>
2026-04-17 03:43:32 +0100

    Update python version in make-venv.sh

--- kobeni/make-venv.sh
+++ kobeni/make-venv.sh
@@ -6,8 +6,8 @@ set -eu
 
 uv sync --locked
 
-ln -sf /usr/bin/python3.13 .venv/bin/python
+ln -sf /usr/bin/python3.14 .venv/bin/python
 ln -sf python .venv/bin/python3
-ln -sf python .venv/bin/python3.13
+ln -sf python .venv/bin/python3.14
 
 sed -i 's:home = /usr/local/bin:/usr/bin:' .venv/pyvenv.cfg
