42dff3e4fd540627d2d4b8fb07da8b7d59a708cb
Joshua Price <git@jprice.uk>
2026-04-17 18:23:54 +0100

    Move logger to root of cli module

--- kobeni/kobeni/cli.py
+++ kobeni/kobeni/cli.py
@@ -10,27 +10,28 @@ from dotenv import load_dotenv
 
 from .mumble import Mumble
 
+logger = logging.getLogger(__name__)
+
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
-        self.logger = logging.getLogger(__name__)
+
         super().__init__(*args, **kwargs)
 
     async def setup_hook(self):
         await self.add_cog(Mumble(self))
 
     async def close(self):
-        self.logger.info("Shutting down bot...")
+        logger.info("Shutting down bot...")
         await super().close()
 
     async def on_ready(self):
-        self.logger.info(f"We have logged in as {self.user}")
+        logger.info(f"We have logged in as {self.user}")
 
 
 def main():
     load_dotenv()
     discord.utils.setup_logging()
-    logger = logging.getLogger(__name__)
 
     intents = discord.Intents.default()
     intents.message_content = True
