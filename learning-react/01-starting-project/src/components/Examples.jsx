5d04f5516b200285b51b261784cfa35c37897d6e
Joshua Price <git@jprice.uk>
2026-07-24 16:56:02 +0100

    Add Section component

--- learning-react/01-starting-project/src/components/Examples.jsx
+++ learning-react/01-starting-project/src/components/Examples.jsx
@@ -1,5 +1,6 @@
 import { EXAMPLES } from "../data";
-import TabButton from "./TabButton.jsx";
+import TabButton from "./TabButton";
+import Section from "./Section";
 import { useState } from "react";
 
 export default function Examples() {
@@ -24,8 +25,7 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
@@ -53,6 +53,6 @@ export default function Examples() {
         </TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
