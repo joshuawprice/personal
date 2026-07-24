29007f4d093922142c25d241bf31917b9bd4b47f
Joshua Price <git@jprice.uk>
2026-07-24 16:58:20 +0100

    Use forwarded props in TabButton

--- learning-react/01-starting-project/src/components/TabButton.jsx
+++ learning-react/01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({ children, isSelected, onClick }) {
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? "active" : null} onClick={onClick}>
+      <button className={isSelected ? "active" : null} {...props}>
         {children}
       </button>
     </li>
